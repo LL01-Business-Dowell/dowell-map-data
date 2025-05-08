dcb7b24e674d7604bd3d5d12360884267f0a0faa
Nitesh Nair <niteshnair@Niteshs-MacBook-Air.local>
2025-05-08 15:30:19 +0530

    removed map view button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ export const Header: React.FC<HeaderProps> = ({
               </div>
             </div>
             
-            <button 
+            {/* <button 
               onClick={() => setMapView(!mapView)}
               className={`px-4 py-2 rounded-lg flex items-center justify-center ${mapView ? 'bg-indigo-800 text-white' : 'bg-white text-indigo-700'}`}
             >
@@ -63,7 +63,7 @@ export const Header: React.FC<HeaderProps> = ({
                 <path fillRule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clipRule="evenodd" />
               </svg>
               {mapView ? 'List View' : 'Map View'}
-            </button>
+            </button> */}
           </motion.div>
         </div>
       </div>
